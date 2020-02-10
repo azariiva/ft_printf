f7967a35b99302e9875cc3b68c4cfeada4a01886
azariiva <zar.isk.rus@gmail.com>
2020-02-10 19:00:56 +0300

    wclen is now ours

--- libft/srcs/ft_wclen.c
+++ libft/srcs/ft_wclen.c
@@ -1,26 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_wclen.c                                         :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2019/03/04 15:07:14 by jvisser        #+#    #+#                */
-/*   Updated: 2019/03/04 15:09:33 by jvisser       ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <wchar.h>
 
-int	ft_wclen(wchar_t wc)
+int	ft_wclen(wchar_t c)
 {
-	if (wc >= 0x00 && wc <= 0x7F)
+	if (c >= 0x00 && c <= 0x7F)
 		return (1);
-	else if (wc >= 0x80 && wc <= 0x7FF)
+	else if (c >= 0x80 && c <= 0x7FF)
 		return (2);
-	else if (wc >= 0x800 && wc <= 0xFFFF)
+	else if (c >= 0x800 && c <= 0xFFFF)
 		return (3);
-	else if (wc >= 0x10000 && wc <= 0x10FFFF)
+	else if (c >= 0x10000 && c <= 0x10FFFF)
 		return (4);
 	return (0);
 }
